7851d90ceaa9bcf5618c03b450cead8d9a0ba976
Hyeonjun Lee <allieclan@gmail.com>
2020-05-27 14:23:36 +0900

    backend-first
    카페 정보 상세화면, 메뉴 출력까지 완료

--- cafesite/cafeyo/urls.py
+++ cafesite/cafeyo/urls.py
@@ -6,6 +6,8 @@ app_name = 'cafeyo'
 urlpatterns = [
     # ex: /cafeyo/
     path('', views.IndexView.as_view(), name='index'),
-    # ex: /cafeyo/북문/
-    path('<int:pk>/', views.DetailView.as_view(), name='detail')
+    # ex: /cafeyo/1/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /cafeyo/1/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results')
 ]
