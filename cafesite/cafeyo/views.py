9df14e014a1f24526a8636de3c45ae45b6b8fbe9
Hyeonjun Lee <allieclan@gmail.com>
2020-05-29 01:36:37 +0900

    add random menu & cafe
    제목그대로입니다..

--- cafesite/cafeyo/views.py
+++ cafesite/cafeyo/views.py
@@ -1,7 +1,10 @@
+import random
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.db.models import Max
 
 from .models import Gate, Cafe, Category, Menu
 
@@ -21,7 +24,31 @@ class DetailView(generic.DetailView):
     model = Gate
     template_name = 'cafeyo/detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        max_id = Cafe.objects.all().aggregate(max_id=Max('id'))['max_id']
+        while True:
+            pk = random.randint(1, max_id)
+            context['random_cafe'] = Cafe.objects.filter(pk=pk).first()
+            if context['random_cafe']:
+                return context
+
 
 class ResultsView(generic.DetailView):
     model = Cafe
     template_name = 'cafeyo/results.html'
+
+    # random menu pick v.2
+    def get_context_data(self, **kwargs):
+        context = super(ResultsView, self).get_context_data(**kwargs)
+        max_id = Menu.objects.all().aggregate(max_id=Max('id'))['max_id']
+        while True:
+            pk = random.randint(1, max_id)
+            context['random_menu'] = Menu.objects.filter(pk=pk).first()
+            if context['random_menu']:
+                return context
+
+    # random menu pick v.1
+    # def get_context_data(self, **kwargs):
+    #     context['random_menu'] = Menu.objects.order_by('?').first()
+    #     return context
