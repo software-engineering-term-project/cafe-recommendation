6310904bedc05ef53edcb7eac1d0c65fc7439356
Hyeonjun Lee <allieclan@gmail.com>
2020-06-04 21:55:44 +0900

    backend - views & urls change
    카테고리 선택 기능에 문제가 있어 수정

--- cafesite/cafeyo/urls.py
+++ cafesite/cafeyo/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # ex: /cafeyo/list/1/
     path('list/<int:gate_id>/', views.CafelistView.as_view(), name='cafelist'),
     # ex: /cafeyo//list/1/category/3/
-    path('list/<int:gate_id>/category/<category_id>/',
+    path('list/<int:gate_id>/category/<int:category_id>/',
          views.CafelistView.as_view(), name='cafelist_category'),
     # ex: /cafeyo/results/1/
     path('results/<int:pk>/', views.ResultsView.as_view(), name='results')
